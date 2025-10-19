426e4c60d722844bf7b51d4bf865f6a66b5b105c
Malse <eslamwtf@gmail.com>
2025-10-19 11:14:50 +0300

    first model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { get } = require('mongoose');
 const router = express.Router();
 
 // @route    GET api/users
 // @desc     Test route
 // @access   Public
-router.get('/', (req, res) => res.send('Users Route'));
+router.get('/api/users', async (req, res) => {
+    try {
+        const users = await users.find({});
+        res.json(users);
+    }catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+}
+});
 
 module.exports = router;
