fbcb5f15bfbfae0e60a5e21ba7bf3cf0aaebc412
Malse <eslamwtf@gmail.com>
2025-10-21 15:20:02 +0300

    jwt authorizations

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 const Router = express.Router();
-
+const User = require('../../models/User');
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-Router.get('/', (req, res) => res.send('Auth Route'));
+Router.get('/',auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        res.status(500).send('Server Error here');
+    }
+});
 
 module.exports = Router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -52,8 +52,22 @@ router.post(
 			// Encrypt password
 			const salt = await bcrypt.genSalt(10);
 			user.password = await bcrypt.hash(password, salt);
-			await user.save();
 
+			const payload = {
+				user: { id: user.id },
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+			
+			await user.save();
 			res.send('User registered');
 		} catch (error) {
 			console.error(error.message);
