05f6a0562d468a5e6f40f59689c09c4480ecf5d4
Malse <eslamwtf@gmail.com>
2025-10-22 15:48:37 +0300

    request user profile data

--- models/Profile.js
+++ models/Profile.js
@@ -106,4 +106,4 @@ const ProfileSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('profile', ProfileSchema);
+module.exports = mongoose.model('profiles', ProfileSchema);
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
-// @route    GET api/Profiles
-// @desc     Test route
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth')
+const User = require('../../models/User');
+// @route    GET api/Profile/me
+// @desc     get current users profile
 // @access   Public
-router.get('/', (req, res) => res.send('Profiles Route'));
+router.get('/me',auth, async (req, res) => {
+    try{
+    const profile = await Profile.findOne({user: req.user.id}).populate("user",['name','avatar'])
+    if(!profile){
+        return res.status(400).json({msg:"there is no profile for this user"})
+    }
+    res.json(profile);
+}catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
