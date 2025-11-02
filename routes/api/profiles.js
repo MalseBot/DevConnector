64f0796a2f550f17faae2751a4f3ce30d346a9cb
Malse <eslamwtf@gmail.com>
2025-11-02 14:34:42 +0300

    fetching repos from github API

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,6 +1,8 @@
 /** @format */
 
 const express = require('express');
+const config = require('config')
+const request = require('request')
 const router = express.Router();
 const Profile = require('../../models/Profile');
 const auth = require('../../middleware/auth');
@@ -276,5 +278,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+
+// @route    Get /github/:username
+// @desc     get github repos
+// @access   Public
+
+router.get('/github/:username',(req,res)=>{
+	try {
+		const options={
+			uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientID")}&client_secret=${config.get('githubClientSecret')}`,
+			method:'GET',
+			headers:{'user-agent' : 'node.js'}
+		}
+		request(options,(error,response,body)=>{
+			if(error) console.log(error);
+
+			if(response.statusCode !== 200) {
+				res.status(404).json({msg:"No gihub profile found"})
+			}else{
+				res.json(JSON.parse(body))
+			}
+			
+		})
+	} catch (error) {
+		console.log(error);
+		res.status(500).send('Server Error')
+		
+	}
+})
 
 module.exports = router;
